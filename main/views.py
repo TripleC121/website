b8ba8b9c95992cb304c14d29b472b1320e5f0165
TripleC121 <168608813+TripleC121@users.noreply.github.com>
2024-08-18 10:32:42 -0500

    still working on fixing the GitHub Actions

--- main/views.py
+++ main/views.py
@@ -1,44 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import login
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
-from .forms import CustomUserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import (
+    PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView
+)
+from .forms import CustomUserCreationFor
 
 
-def homepage(request):
+def home(request):
     return render(request, 'index.html')
 
 
-def learning(request):
-    return render(request, 'learning/index.html')
-
-
-def full_stack(request):
-    return render(request, 'learning/full_stack/index.html')
-
-
-def pgp_aiml(request):
-   return render(request, 'learning/pgp_aiml/index.html')
-
-
-def project1(request):
-   return render(request, 'learning/pgp_aiml/project1.html')
-
-
-def project2(request):
-   return render(request, 'learning/pgp_aiml/project2.html')
-
-
-def project3(request):
-   return render(request, 'learning/pgp_aiml/project3.html')
-
-
-def project4(request):
-   return render(request, 'learning/pgp_aiml/project4.html')
-
-
-def project5(request):
-   return render(request, 'learning/pgp_aiml/project5.html')
+def about(request):
+    return render(request, 'about/index.html')
 
 
 def growing(request):
@@ -50,18 +26,39 @@ def succulents(request):
 
 
 def trees(request):
-   return render(request, 'growing/trees/index.html')
+    return render(request, 'growing/trees/index.html')
 
 
-def about(request):
-    return render(request, 'about/index.html')
+def learning(request):
+    return render(request, 'learning/index.html')
 
 
-def user_test(request):
-    return render(request, 'user_test/index.html')
+def full_stack(request):
+    return render(request, 'learning/full_stack/index.html')
 
-# user authentication
-from .forms import CustomUserCreationForm
+
+def pgp_aiml(request):
+    return render(request, 'learning/pgp_aiml/index.html')
+
+
+def project1(request):
+    return render(request, 'learning/pgp_aiml/project1.html')
+
+
+def project2(request):
+    return render(request, 'learning/pgp_aiml/project2.html')
+
+
+def project3(request):
+    return render(request, 'learning/pgp_aiml/project3.html')
+
+
+def project4(request):
+    return render(request, 'learning/pgp_aiml/project4.html')
+
+
+def project5(request):
+    return render(request, 'learning/pgp_aiml/project5.html')
 
 
 def signup(request):
@@ -70,16 +67,18 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, 'Account created successfully. Welcome!')
             return redirect('homepage')
-        else:
-            messages.error(request, 'There was an error with your registration. Please check the form and try again.')
     else:
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
 
-# password reset
+@login_required
+def user_test(request):
+    return render(request, 'user_test/index.html')
+
+
+# Password reset views
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'password_reset/password_reset_form.html'
     email_template_name = 'password_reset/password_reset_email.html'
@@ -99,7 +98,6 @@ class CustomPasswordResetConfirmView(PasswordResetConfirmView):
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     template_name = 'password_reset/password_reset_complete.html'
 
-# def exercise(request):
-#   return render(request, 'exercise/index.html')
 
-# Add more view functions as needed
+# def exercise(request):
+#     return render(request, 'exercise/index.html')
