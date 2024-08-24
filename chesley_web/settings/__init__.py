73b1b9da06decf80805eac666af335249994ab44
TripleC121 <168608813+TripleC121@users.noreply.github.com>
2024-08-24 11:58:23 -0500

    updated init.py to fix produciton issue

--- chesley_web/settings/__init__.py
+++ chesley_web/settings/__init__.py
@@ -1,12 +1,18 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 from .base import *  # noqa: F403
 
 environment = os.environ.get('DJANGO_ENVIRONMENT', 'development')
+logger.debug(f"DJANGO_ENVIRONMENT: {environment}")
+logger.debug(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
 
 if environment == 'production':
+    logger.debug("Loading production settings")
     from .production import *  # noqa: F403, F401
 else:
+    logger.debug("Loading development settings")
     from .development import *  # noqa: F403, F401
-
-# Optionally, you can add a print statement for debugging:
-# print(f"Running in {environment} environment")
