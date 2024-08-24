f2ea869ebeaa71025c0fbb2586bdef6d51de2d30
TripleC121 <168608813+TripleC121@users.noreply.github.com>
2024-08-24 12:24:00 -0500

    Updated init.py

--- chesley_web/settings/__init__.py
+++ chesley_web/settings/__init__.py
@@ -6,11 +6,10 @@ logger = logging.getLogger(__name__)
 
 from .base import *  # noqa: F403
 
-environment = os.environ.get('DJANGO_ENVIRONMENT', 'development')
-logger.debug(f"DJANGO_ENVIRONMENT: {environment}")
-logger.debug(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
+environment = os.environ.get('DJANGO_SETTINGS_MODULE', 'chesley_web.settings.development')
+logger.debug(f"DJANGO_SETTINGS_MODULE: {environment}")
 
-if environment == 'production':
+if environment == 'chesley_web.settings.production':
     logger.debug("Loading production settings")
     from .production import *  # noqa: F403, F401
 else:
