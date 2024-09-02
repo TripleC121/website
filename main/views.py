3470409a333ed16f36d7f778531a2bdecffc67da
triplec <168608813+TripleC121@users.noreply.github.com>
2024-09-02 08:25:35 -0500

    fixed syntax issues in views.py

--- main/views.py
+++ main/views.py
@@ -14,68 +14,90 @@ logger = logging.getLogger(__name__)
 def homepage(request):
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about/index.html')
+
 
 def growing(request):
     return render(request, 'growing/index.html')
 
+
 def succulents(request):
     return render(request, 'growing/succulents/index.html')
+
 
 def trees(request):
     return render(request, 'growing/trees/index.html')
 
+
 def learning(request):
     return render(request, 'learning/index.html')
+
 
 def full_stack(request):
     return render(request, 'learning/full_stack/index.html')
 
+
 def cloud_tech (request):
     return render(request, 'learning/full_stack/cloud_tech.html')
+
 
 def devops_overview (request):
     return render(request, 'learning/full_stack/devops_overview.html')
 
+
 def django_details (request):
     return render(request, 'learning/full_stack/django_details.html')
+
 
 def frontend_overview(request):
     return render(request, 'learning/full_stack/frontend_overview.html')
 
+
 def pgp_aiml(request):
     return render(request, 'learning/pgp_aiml/index.html')
+
 
 def project1(request):
     return render(request, 'learning/pgp_aiml/project1.html')
 
+
 def project2(request):
     return render(request, 'learning/pgp_aiml/project2.html')
+
 
 def project3(request):
     return render(request, 'learning/pgp_aiml/project3.html')
 
+
 def project4(request):
     return render(request, 'learning/pgp_aiml/project4.html')
+
 
 def project5(request):
     return render(request, 'learning/pgp_aiml/project5.html')
 
+
 def technology_view(request):
     return render(request, 'technology/index.html')
+
 
 def cloud_technologies_view(request):
     return render(request, 'technology/cloud_technologies.html')
 
+
 def devops_cicd_view(request):
     return render(request, 'technology/devops_cicd.html')
+
 
 def backend_technologies_view(request):
     return render(request, 'technology/backend_technologies.html')
 
+
 def frontend_technologies_view(request):
     return render(request, 'technology/frontend_technologies.html')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -88,9 +110,11 @@ def signup(request):
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
+
 @login_required
 def user_test(request):
     return render(request, 'user_test/index.html')
+
 
 # Password reset views
 class CustomPasswordResetView(PasswordResetView):
@@ -99,12 +123,15 @@ class CustomPasswordResetView(PasswordResetView):
     subject_template_name = 'password_reset/password_reset_subject.txt'
     success_url = '/password_reset/done/'
 
+
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     template_name = 'password_reset/password_reset_done.html'
+
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'password_reset/password_reset_confirm.html'
     success_url = '/password_reset/complete/'
+
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     template_name = 'password_reset/password_reset_complete.html'
