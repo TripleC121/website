a8f3cda71508643ab3072bd6a07fcfe48ae88706
triplec <168608813+TripleC121@users.noreply.github.com>
2024-09-02 08:15:07 -0500

    Updated learning pages to be learning focused and added technology pages to be technology focused

--- main/urls.py
+++ main/urls.py
@@ -1,26 +1,29 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from . import views
-from .views import TechnologyView, CICDDetailsView
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('learning/', views.learning, name='learning'),
     path('learning/full_stack/', views.full_stack, name='full_stack'),
+    path('learning/full_stack/cloud_tech', views.cloud_tech, name='cloud_tech'),
+    path('learning/full_stack/devops_overview', views.devops_overview, name='devops_overview'),
+    path('learning/full_stack/frontend_overview', views.frontend_overview, name='frontend_overview'),
+    path('learning/full_stack/django_details', views.django_details, name='django_details'),
     path('learning/pgp_aiml/', views.pgp_aiml, name='pgp_aiml'),
-    path('learning/full_stack/frontend/', views.frontend_overview, name='frontend_overview'),
-    path('learning/full_stack/cloud/', views.cloud_tech, name='cloud_tech'),
-    path('learning/full_stack/django/', views.django_details, name='django_details'),
-    path('learning/full_stack/devops/', views.devops_overview, name='devops_overview'),
-    path('learning/pgp_aiml/foundations/', views.ai_foundations, name='ai_foundations'),
-    path('learning/pgp_aiml/machine-learning/', views.machine_learning, name='machine_learning'),
-    path('learning/pgp_aiml/deep-learning/', views.deep_learning, name='deep_learning'),
-    path('learning/pgp_aiml/projects/', views.course_projects, name='course_projects'),
     path('learning/pgp_aiml/project1', views.project1, name='project1'),
     path('learning/pgp_aiml/project2', views.project2, name='project2'),
     path('learning/pgp_aiml/project3', views.project3, name='project3'),
     path('learning/pgp_aiml/project4', views.project4, name='project4'),
     path('learning/pgp_aiml/project5', views.project5, name='project5'),
+    path('technology/', views.technology_view, name='technology'),
+    path('technology/cloud/', views.cloud_technologies_view, name='cloud_technologies'),
+    path('technology/devops-cicd/', views.devops_cicd_view, name='devops_cicd'),
+    path('technology/backend/', views.backend_technologies_view, name='backend_technologies'),
+    path('technology/frontend/', views.frontend_technologies_view, name='frontend_technologies'),
+    path('technology/', views.technology_view, name='technology'),
+    path('technology/cloud/', views.cloud_technologies_view, name='cloud_technologies'),
+    path('technology/devops-cicd/', views.devops_cicd_view, name='devops_deployment'),
     path('growing/', views.growing, name='growing'),
     path('growing/succulents/', views.succulents, name='succulents'),
     path('growing/trees/', views.trees, name='trees'),
@@ -33,6 +36,4 @@ urlpatterns = [
     path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('technology/', TechnologyView.as_view(), name='technology'),
-    path('technology/cicd/', CICDDetailsView.as_view(), name='cicd_details'),
 ]
--- main/views.py
+++ main/views.py
@@ -11,99 +11,71 @@ from .forms import CustomUserCreationForm
 import logging
 logger = logging.getLogger(__name__)
 
-
 def homepage(request):
     return render(request, 'index.html')
-
 
 def about(request):
     return render(request, 'about/index.html')
 
-
 def growing(request):
     return render(request, 'growing/index.html')
-
 
 def succulents(request):
     return render(request, 'growing/succulents/index.html')
 
-
 def trees(request):
     return render(request, 'growing/trees/index.html')
-
 
 def learning(request):
     return render(request, 'learning/index.html')
 
+def full_stack(request):
+    return render(request, 'learning/full_stack/index.html')
+
+def cloud_tech (request):
+    return render(request, 'learning/full_stack/cloud_tech.html')
+
+def devops_overview (request):
+    return render(request, 'learning/full_stack/devops_overview.html')
+
+def django_details (request):
+    return render(request, 'learning/full_stack/django_details.html')
 
 def frontend_overview(request):
     return render(request, 'learning/full_stack/frontend_overview.html')
 
-
-def cloud_tech(request):
-    return render(request, 'learning/full_stack/cloud_tech.html')
-
-
-def django_details(request):
-    return render(request, 'learning/full_stack/django_details.html')
-
-
-def devops_overview(request):
-    return render(request, 'learning/full_stack/devops_overview.html')
-
-
-# New view functions for PGP-AIML subpages
-def ai_foundations(request):
-    return render(request, 'learning/pgp_aiml/ai_foundations.html')
-
-
-def machine_learning(request):
-    return render(request, 'learning/pgp_aiml/machine_learning.html')
-
-
-def deep_learning(request):
-    return render(request, 'learning/pgp_aiml/deep_learning.html')
-
-
-def course_projects(request):
-    return render(request, 'learning/pgp_aiml/course_projects.html')
-
-
-def full_stack(request):
-    return render(request, 'learning/full_stack/index.html')
-
-
 def pgp_aiml(request):
     return render(request, 'learning/pgp_aiml/index.html')
-
 
 def project1(request):
     return render(request, 'learning/pgp_aiml/project1.html')
 
-
 def project2(request):
     return render(request, 'learning/pgp_aiml/project2.html')
-
 
 def project3(request):
     return render(request, 'learning/pgp_aiml/project3.html')
 
-
 def project4(request):
     return render(request, 'learning/pgp_aiml/project4.html')
-
 
 def project5(request):
     return render(request, 'learning/pgp_aiml/project5.html')
 
+def technology_view(request):
+    return render(request, 'technology/index.html')
 
-class TechnologyView(TemplateView):
-    template_name = 'technology/index.html'
+def cloud_technologies_view(request):
+    return render(request, 'technology/cloud_technologies.html')
 
+def devops_cicd_view(request):
+    return render(request, 'technology/devops_cicd.html')
 
-class CICDDetailsView(TemplateView):
-    template_name = 'technology/cicd_details.html'
+def backend_technologies_view(request):
+    return render(request, 'technology/backend_technologies.html')
 
+def frontend_technologies_view(request):
+    return render(request, 'technology/frontend_technologies.html')
 
 def signup(request):
     if request.method == 'POST':
@@ -116,11 +88,9 @@ def signup(request):
         form = CustomUserCreationForm()
     return render(request, 'signup.html', {'form': form})
 
-
 @login_required
 def user_test(request):
     return render(request, 'user_test/index.html')
-
 
 # Password reset views
 class CustomPasswordResetView(PasswordResetView):
@@ -129,19 +99,12 @@ class CustomPasswordResetView(PasswordResetView):
     subject_template_name = 'password_reset/password_reset_subject.txt'
     success_url = '/password_reset/done/'
 
-
 class CustomPasswordResetDoneView(PasswordResetDoneView):
     template_name = 'password_reset/password_reset_done.html'
-
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'password_reset/password_reset_confirm.html'
     success_url = '/password_reset/complete/'
 
-
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
     template_name = 'password_reset/password_reset_complete.html'
-
-
-# def exercise(request):
-#     return render(request, 'exercise/index.html')
