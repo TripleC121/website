0340a8eae14e0e65129b8def4b92043e5ad8c508
triplec <168608813+TripleC121@users.noreply.github.com>
2024-09-10 19:34:08 -0500

    Working on fixing S3 permission errors

--- chesley_web/settings/production.py
+++ chesley_web/settings/production.py
@@ -37,8 +37,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_DEFAULT_ACL = 'public-read'
-AWS_region = 'us-east-1'
 
 # logging
 # print(f"Database settings: {DATABASES}")
