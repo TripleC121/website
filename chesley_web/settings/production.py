41140ed021cbd9bf9f2fb72c6ded98721704fcd6
triplec <168608813+TripleC121@users.noreply.github.com>
2024-11-01 06:46:50 -0500

    test: triggering CICD workflow

--- chesley_web/settings/production.py
+++ chesley_web/settings/production.py
@@ -2,7 +2,7 @@ import sys
 
 from .base import *
 
-# version 0.2
+# version 0.2.1
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env.prod"))
