8eb8b5671af35284f7f4b54d162c643506e94547
triplec <168608813+TripleC121@users.noreply.github.com>
2024-12-05 07:19:57 -0600

    Added static file collection. removed gunicorn.conf.py from .gitignore. Turned off debug in Docker.

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+bind = "unix:/opt/website/run/chesley_web.sock"
+workers = 3
+user = "webapps"
+group = "www-data"
+
+limit_request_line = 4094
+limit_request_fields = 100
+limit_request_field_size = 8190
+timeout = 30
+keepalive = 2
+
+accesslog = "/var/log/chesley_web/django/access.log"
+errorlog = "/var/log/chesley_web/django/error.log"
